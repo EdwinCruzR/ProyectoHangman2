02c4d617de18053fee383ef6fd2bf79eb6847d68
Edwin <bomboncinedwin@gmail.com>
2023-11-15 20:54:37 -0600

    Editar y eliminar de palabras

---
+++ php/eliminarPalabras.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+
+include("../bd/conexion.php");
+if(!isset($_SESSION['id'])){
+ header("Location: ../index.php");
+}
+ 
+$cod = $_GET['id'];
+ 
+mysqli_query($conexion, "DELETE FROM word WHERE id=$cod");
+ 
+header("Location:dashpage.php");
+
+?>
