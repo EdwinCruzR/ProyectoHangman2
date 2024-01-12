808f92c148397b40d0cf31e2d72546c7a96c3cbe
F1oxyz <angelafloxz@gmail.com>
2024-01-11 19:31:51 -0600

    a

--- php/eliminarPalabras.php
+++ php/eliminarPalabras.php
@@ -1,14 +1,16 @@
 <?php
+
 session_start();
 
 include("../bd/conexion.php");
 if(!isset($_SESSION['id'])){
  header("Location: ../index.php");
 }
- 
+$iduser = $_SESSION['id'];
+
 $cod = $_GET['id'];
  
-mysqli_query($conexion, "UPDATE words SET isactive=0 WHERE id=$id");
+mysqli_query($conexion, "UPDATE words SET isactive=0 WHERE id=$id AND user_id=$iduser");
  
 header("Location:dashpage.php");
 
