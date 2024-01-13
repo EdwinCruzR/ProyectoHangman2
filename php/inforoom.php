4033f91b9f25aa6bd37a1aa7d5e1b3afa2e3156b
F1oxyz <angelafloxz@gmail.com>
2024-01-12 22:53:54 -0600

    ajuste de requerimientos

--- php/inforoom.php
+++ php/inforoom.php
@@ -14,6 +14,7 @@ $id = $_GET['id'];
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.rawgit.com/davidshimjs/qrcodejs/gh-pages/qrcode.min.js"></script>
     <link href="../assets/bootstrap/themes/sketchy/bootstrap.css" rel="stylesheet">
     <link rel="stylesheet" href="../assets/css/editar.css">
     <title>Tu sala</title>
@@ -37,7 +38,7 @@ $id = $_GET['id'];
         th,
         td {
             border: 1px solid black;
-            padding: 8px;
+            padding: 9px;
             text-align: left;
         }
     </style>
@@ -53,7 +54,29 @@ $id = $_GET['id'];
                 <?php
                 $consulta_salas = mysqli_query($conexion, "SELECT * FROM room where id='$id'");
                 while ($row = mysqli_fetch_array($consulta_salas)):
-                    ?>
+                    $roomid = $row['id'];
+                    $roomcode = $row['roomcode'];
+                    $qrstring = $row['qrstring'];
+                    $roomname = $row['roomname'];
+                    $isopen = $row['isopen'];
+                    $timestamp = $row['timestamp'];
+                    $hasstartdatetime = $row['hasstartdatetime'];
+                    $startdatetime = $row['startdatetime'];
+                    $hasenddatetime = $row['hasenddatetime'];
+                    $enddatetime = $row['enddatetime'];
+                endwhile;
+                
+                $consulta_gameroom = mysqli_query($conexion, "SELECT * FROM gameroom where room_id='$roomid'");
+                $numero_filas_jugadores = mysqli_num_rows($consulta_gameroom);
+
+                $consulta_rhw = mysqli_query($conexion, "SELECT * FROM room_has_word where room_id='$roomid'");
+                $numero_filas_palabras = mysqli_num_rows($consulta_rhw);
+                
+                // while ($row = mysqli_fetch_array($consulta_salas)):
+                //     $roomcode = $row['roomcode'];
+
+                // endwhile;
+                ?>
                     <div class="tabla1">
                     <table class="tabla">
                         <caption>Info. Sala</caption>
@@ -65,69 +88,81 @@ $id = $_GET['id'];
                         </thead>
                         <tbody>
                             <tr>
+                                <td>Codigo de sala</td>
+                                <td><?= $roomcode ?></td>
+                            </tr>
+                            <tr>
                                 <td>Nombre</td>
                                 <td>
-                                    <?= $row['roomname'] ?>
+                                    <?= $roomname ?>
                                 </td>
                             </tr>
                             <tr>
-                                <td>Descripcion</td>
+                                <td>Jugadores que participaron</td>
+                                <td><?= $numero_filas_jugadores ?></td>
+                            </tr>
+                            <tr>
+                                <td>Numero de palabras</td>
+                                <td><?= $numero_filas_palabras ?></td>
+                            </tr>
+                            <tr>
+                                <td>QR</td>
                                 <td>
-                                    <?= $row['description'] ?>
+                                <div id="qrcode"></div>
+                                <script>
+                                    var contenidoQR = '<?= $qrstring ?>';
+                                    var qrcode = new QRCode(document.getElementById("qrcode"), {
+                                        text: contenidoQR,
+                                        width: 100,
+                                        height: 100,
+                                        colorDark : "#000000",
+                                        colorLight : "#ffffff",
+                                        correctLevel : QRCode.CorrectLevel.L
+                                    });
+                                </script>
                                 </td>
                             </tr>
                             <tr>
-                                <td>Vidas</td>
-                                <td>
-                                    <?= (($row['lives'] == -1) ? "ilimitadas" : $row['lives']) ?>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>Pistas</td>
-                                <td>
-                                    <?= (($row['clue'] == 1) ? "Si" : "No") ?>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>pista despues de:
-                                </td>
-                                <td>
-                                    <?php echo $row['clueafter'] ?> intentos
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>Retroalimentación
-                                </td>
+                                <td>Abierto</td>
                                 <th>
-                                    <?= (($row['feedback'] == 1) ? "Si" : "No") ?>
+                                    <?= (($isopen == 1) ? "Si" : "No") ?>
                                 </th>
                             </tr>
                             <tr>
-                                <td>Random
-                                </td>
+                                <td>Creado el:</td>
                                 <th>
-                                    <?= (($row['random'] == 1) ? "Si" : "No") ?>
+                                    <?= $timestamp ?>
                                 </th>
                             </tr>
                             <tr>
-                                <td>Lista
-                                </td>
+                                <td>Hora de apertura?</td>
                                 <th>
-                                    <?= (($row['isgeneral'] == 1) ? "Sistema" : "Lista") ?>
+                                    <?= (($hasstartdatetime == 1) ? "Si" : "No") ?>
                                 </th>
                             </tr>
                             <tr>
-                                <td>Abierto
-                                </td>
+                                <td>Hora apertura</td>
                                 <th>
-                                    <?= (($row['isopen'] == 1) ? "Si" : "No") ?>
+                                    <?= $startdatetime ?>
                                 </th>
                             </tr>
-                            <!-- Agrega más filas según sea necesario -->
+                            <tr>
+                                <td>Hora de cierre?</td>
+                                <th>
+                                    <?= (($hasenddatetime == 1) ? "Si" : "No") ?>
+                                </th>
+                            </tr>
+                            <tr>
+                                <td>Hora cierre</td>
+                                <th>
+                                    <?= $enddatetime ?>
+                                </th>
+                            </tr>
                         </tbody>
                     </table>
                     </div>
 
+                    
 
                     <div class="tabla2">
                     <!-- Segunda tabla -->
@@ -137,26 +172,46 @@ $id = $_GET['id'];
                         <thead>
                             <tr>
                                 <th>#</th>
-                                <th>Avatar</th>
                                 <th>Nombre</th>
                                 <th>Puntos</th>
                                 <th>Tiempo</th>
-                                <th>Observ</th>
+                                <th>acciones</th>
                             </tr>
                         </thead>
                         <tbody id="players" class="table-group-divider">
+                            <?php 
+                            $posi = 0;
+                            $consulta_players = mysqli_query($conexion, "SELECT * FROM gameroom JOIN users ON gameroom.user_id = users.id WHERE gameroom.room_id = $roomid ORDER BY score DESC");
+                            // $consulta_players = mysqli_query($conexion, "SELECT * FROM gameroom WHERE room_id = $roomid ORDER BY score DESC");
+                            while ($row = mysqli_fetch_array($consulta_players)):
+                                $posi++;
+                                $idplayer = $row['user_id'];
+                                $nombre = $row['name'];
+                                $score = $row['score'];
+                                $tiempo = $row['totaltime'];
+                            ?>
                             <tr>
-                                <td>dad</td>
+                                <td><?= $posi ?></td>
+                                <td><?= $nombre ?></td>
+                                <td><?= $score ?></td>
+                                <td><?= $tiempo ?></td>
+                                <td>
+                                    <a href="#" class="users-table--more" >Ver mas</a>
+                                    <!-- aqui que muesre un modal con otra tabla de palabras  -->
+                                </td>
                             </tr>
+                            <?php 
+                            endwhile;
+                            ?>
                         </tbody>
                     </table>
-                <?php
-                endwhile;
-                ?>
-                </div>
+                    </div>
+                    </div>
+                    <a href="#"><button type="button" class="btn btn-success regresar">lista de palabras</button></a>
+                    <a href="#"><button type="button" class="btn btn-success regresar">palabras ordenadas </button></a>
+                    <a href="#"><button type="button" class="btn btn-success regresar">palabras que no le aparecieron</button></a>
                 </div>
             </div>
-        </div>
 
     </div>
 </body>
