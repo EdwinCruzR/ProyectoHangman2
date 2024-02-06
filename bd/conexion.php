f974209813fd6bc224f4305746e2c42c137be919
F1oxyz <angelafloxz@gmail.com>
2024-02-06 16:32:41 -0600

    conex

--- bd/conexion.php
+++ bd/conexion.php
@@ -1,12 +1,7 @@
 <?php
-/*$servidor = "65.99.225.56";
+$servidor = "65.99.225.56";
 $usuario = "cbtised3_hangman";
 $contrasenia      = "vGVWQ_Ljlm*K";
 $nombreBaseDatos = "cbtised3_hangman";
-$conexion = new mysqli($servidor, $usuario, $contrasenia, $nombreBaseDatos);*/
-$servidor = "localhost";
-$usuario = "root";
-$contrasenia= "";
-$nombreBaseDatos = "hangman";
 $conexion = new mysqli($servidor, $usuario, $contrasenia, $nombreBaseDatos);
 ?>
