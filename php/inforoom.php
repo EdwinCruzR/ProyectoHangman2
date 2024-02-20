ed8d92238b45f06f8bc2ada32beff0345d499947
F1oxyz <angelafloxz@gmail.com>
2024-02-19 18:44:07 -0600

    a

--- php/inforoom.php
+++ php/inforoom.php
@@ -15,7 +15,7 @@ $id = $_GET['id'];
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="../assets/bootstrap/themes/sketchy/bootstrap.css" rel="stylesheet">
-    <link rel="stylesheet" href="../assets./css/inforoom.css">
+    <link rel="stylesheet" href="../assets/css/inforoom.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
     <script src="https://cdn.rawgit.com/davidshimjs/qrcodejs/gh-pages/qrcode.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
